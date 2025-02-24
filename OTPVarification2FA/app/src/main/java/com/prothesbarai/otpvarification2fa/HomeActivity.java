1318619f3d22996d482db6e0048358c3f7928b15
prothesbarai <aspprothesbarai1619@gmail.com>
2025-02-24 20:47:46 +0600

    v1.0.6

--- OTPVarification2FA/app/src/main/java/com/prothesbarai/otpvarification2fa/HomeActivity.java
+++ OTPVarification2FA/app/src/main/java/com/prothesbarai/otpvarification2fa/HomeActivity.java
@@ -1,10 +1,20 @@
 package com.prothesbarai.otpvarification2fa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class HomeActivity extends AppCompatActivity {
+    private String[] countryCode;
+    private AutoCompleteTextView countryCodes;
+    private TextInputEditText phnNumberField;
+    private AppCompatButton sendOTPBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +22,23 @@ public class HomeActivity extends AppCompatActivity {
         this.getWindow().setStatusBarColor(getResources().getColor(R.color.c5));
         this.getWindow().setNavigationBarColor(getResources().getColor(R.color.c5));
         setContentView(R.layout.home_activity);
+
+        countryCodes = findViewById(R.id.countryCodes);
+        phnNumberField = findViewById(R.id.phnNumberField);
+        sendOTPBtn = findViewById(R.id.sendOTPBtn);
+        countryCode = getResources().getStringArray(R.array.country_code);
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter<>(this,R.layout.country_code_items,R.id.countryCodeItems,countryCode);
+        countryCodes.setAdapter(arrayAdapter);
+
+
+        sendOTPBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+
     }
 }
