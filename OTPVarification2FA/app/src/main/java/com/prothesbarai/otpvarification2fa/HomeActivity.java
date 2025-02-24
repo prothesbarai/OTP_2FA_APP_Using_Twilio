0c5e7cc8440e434864dc80c7e4b6a3e600e6555a
prothesbarai <aspprothesbarai1619@gmail.com>
2025-02-24 21:17:57 +0600

    v2.0.7

--- OTPVarification2FA/app/src/main/java/com/prothesbarai/otpvarification2fa/HomeActivity.java
+++ OTPVarification2FA/app/src/main/java/com/prothesbarai/otpvarification2fa/HomeActivity.java
@@ -7,14 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.TextView;
 
 import com.google.android.material.textfield.TextInputEditText;
+
+import java.lang.reflect.Field;
+import java.util.Random;
 
 public class HomeActivity extends AppCompatActivity {
     private String[] countryCode;
     private AutoCompleteTextView countryCodes;
     private TextInputEditText phnNumberField;
     private AppCompatButton sendOTPBtn;
+    private TextView showOTPField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +31,7 @@ public class HomeActivity extends AppCompatActivity {
         countryCodes = findViewById(R.id.countryCodes);
         phnNumberField = findViewById(R.id.phnNumberField);
         sendOTPBtn = findViewById(R.id.sendOTPBtn);
+        showOTPField = findViewById(R.id.showOTPField);
         countryCode = getResources().getStringArray(R.array.country_code);
 
         ArrayAdapter arrayAdapter = new ArrayAdapter<>(this,R.layout.country_code_items,R.id.countryCodeItems,countryCode);
@@ -35,7 +41,50 @@ public class HomeActivity extends AppCompatActivity {
         sendOTPBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+                if(countryCodes.getText().length()>0 && phnNumberField.getText().length()>0){
+                    int otpLength = 6;
+                    String otp = "";
+                    // Get Input Field CC & Phn no
+                    String getCC = countryCodes.getText().toString();
+                    String getPhnNo = phnNumberField.getText().toString().trim();
+                    // ================ Generate OTP Start Here =====================
+                    Random random = new Random();
+                    StringBuilder otpBuilder = new StringBuilder();
+                    for(int i=0; i<otpLength; i++){
+                        otpBuilder.append(random.nextInt(10));
+                    }
+                    otp = otpBuilder.toString();
+                    showOTPField.setText(otp);
+                    showOTPField.setVisibility(View.VISIBLE);
+                    // ================ Generate OTP End Here =======================
+
+
+
+
+
+                    countryCodes.setError(null);
+                    phnNumberField.setError(null);
+                } else if (countryCodes.getText().length()>0) {
+                    countryCodes.setError(null);
+                    if (phnNumberField.getText().length()>0){
+                        phnNumberField.setError(null);
+                    }else{
+                        phnNumberField.setError("Empty");
+                        showOTPField.setVisibility(View.GONE);
+                    }
+                } else if (phnNumberField.getText().length()>0) {
+                    phnNumberField.setError(null);
+                    if (countryCodes.getText().length()>0){
+                        countryCodes.setError(null);
+                    }else{
+                        countryCodes.setError("Empty");
+                        showOTPField.setVisibility(View.GONE);
+                    }
+                } else{
+                    countryCodes.setError("Empty");
+                    phnNumberField.setError("Empty");
+                    showOTPField.setVisibility(View.GONE);
+                }
             }
         });
 
